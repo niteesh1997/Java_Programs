4d3146d968e9d4cc7da805f66fcf7ff6a670fc72
cool <cool@Abhi>
2024-08-18 18:46:40 +0530

    swap program

--- src/basic/Hello_World.java
+++ src/basic/Hello_World.java
@@ -3,6 +3,8 @@ package basic;
 public class Hello_World {
 
 	public static void main(String[] args) {
+		String personal="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";
+		String cg="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";
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 	}
