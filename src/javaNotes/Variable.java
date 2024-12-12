b7555aa6888bbb1aaa82776223cbfa3bce2d2af4
nhanchan <nhanchan@LIN-5CG1340X4T.corp.capgemini.com>
2024-12-12 14:32:56 +0530

    12/12

---
+++ src/javaNotes/Variable.java
@@ -0,0 +1,19 @@
+package javaNotes;
+
+
+public class Variable {
+
+	public static void main(String[] args) 
+	{
+		int a ;  //declaration
+		a=0;
+		char b='b';
+		String name="Niteesh";
+		int Roll=112;
+		char grade='a';
+		double sal=20.22;
+		
+		System.out.println("name is "+name+" roll number is "+Roll+" grade is "+grade+" salary is "+sal);
+	}
+
+}
